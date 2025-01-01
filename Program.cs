c3b2bd27b0b91e874773c0833d5b1a603a157820
20100 <mmmelkady23@gmail.com>
2025-01-01 20:27:28 +0200

    Answer one Enum

--- Program.cs
+++ Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+        #region Q1 Enum WeekDay
+        enum weekDays
+        {
+            MonDay,
+            TuesDay,
+            WednesDay,
+            ThursDay,
+            FriDay,
+            SaturDay,
+            SunDay
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1: create Enum call WeekDays
+           foreach (weekDays week in Enum.GetValues(typeof(weekDays)))
+                Console.WriteLine(week);
+
+           // Another solve using var and GetNames
+           foreach( var week in Enum.GetNames(typeof(weekDays)))
+                Console.WriteLine(week);
+
+            #endregion
 
         }
     }
