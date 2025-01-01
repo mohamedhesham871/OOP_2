82dee6eb4e059191a82def1437e5d5d013ba4745
20100 <mmmelkady23@gmail.com>
2025-01-01 20:49:57 +0200

    Answer 2 strcut Person

--- Program.cs
+++ Program.cs
@@ -2,28 +2,51 @@
 {
     internal class Program
     {
-        #region Q1 Enum WeekDay
-        enum weekDays
+        //#region Q1 Enum WeekDay
+        //enum weekDays
+        //{
+        //    MonDay,
+        //    TuesDay,
+        //    WednesDay,
+        //    ThursDay,
+        //    FriDay,
+        //    SaturDay,
+        //    SunDay
+        //}
+        //#endregion
+
+        #region Q2 : Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. 
+        //      Then, write a C# program to display the details of all the persons in the array
+        struct person
         {
-            MonDay,
-            TuesDay,
-            WednesDay,
-            ThursDay,
-            FriDay,
-            SaturDay,
-            SunDay
+            public string Name;
+            public int Age;
         }
         #endregion
-
         static void Main(string[] args)
         {
             #region Q1: create Enum call WeekDays
-           foreach (weekDays week in Enum.GetValues(typeof(weekDays)))
-                Console.WriteLine(week);
+            //foreach (weekDays week in Enum.GetValues(typeof(weekDays)))
+            //     Console.WriteLine(week);
 
-           // Another solve using var and GetNames
-           foreach( var week in Enum.GetNames(typeof(weekDays)))
-                Console.WriteLine(week);
+            //// Another solve using var and GetNames
+            //foreach( var week in Enum.GetNames(typeof(weekDays)))
+            //     Console.WriteLine(week);
+
+            #endregion
+            #region Q2: struct call person
+            person[] person =  new person[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Enter Name of prson {i + 1}");
+                person[i].Name = Console.ReadLine();
+                Console.WriteLine($"Enter Age of Person {i + 1}");
+                person[i].Age = int.Parse(Console.ReadLine());
+            }
+            foreach (person i in person)
+            {
+                Console.WriteLine($"Student Name is : {i.Name} and Age is {i.Age}");
+            }
 
             #endregion
 
